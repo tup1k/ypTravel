f2e6593aae5d18253492901f6101866871355253
Oleg K <tup1k@yandex.ru>
2025-01-09 22:35:45 +0300

    Initial Commit

---
+++ ypTravel/ypTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ypTravelApp.swift
+//  ypTravel
+//
+//  Created by Олег Кор on 09.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ypTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
